e180c8db1a294185de25cbeb2c4f38b9f775f9f7
Yuzhi <1139018783@qq.com>
2017-09-08 08:56:05 +0800

    krys整改后的版本

--- HttpIoManipulator.cc
+++ HttpIoManipulator.cc
@@ -22,17 +22,19 @@ void HttpIoManipulator::sendForApproval(const QStringList &path, const QVariant 
 QVariant HttpIoManipulator::pullData(const QString &data)
 {
 
-    const auto res = http_get (ip_, "/cgi-bin/pull-data", {{"type", binary_to_base64 (data.toStdString ())}}, port_);
-    qDebug () << __PRETTY_FUNCTION__ << " " << data;
-    qDebug () << res.data ();
-    QByteArray arr (res.data (), static_cast<int> (res.size ()));
-    return QJsonDocument::fromJson (arr).toVariant ();
+//    const auto res = http_get (ip_, "/cgi-bin/pull-data", {{"type", binary_to_base64 (data.toStdString ())}}, port_);
+//    qDebug () << __PRETTY_FUNCTION__ << " " << data;
+//    qDebug () << res.data ();
+//    QByteArray arr (res.data (), static_cast<int> (res.size ()));
+//    return QJsonDocument::fromJson (arr).toVariant ();
+    return {};
 }
 
 bool HttpIoManipulator::pushData (const QString &type, const QVariant &data)
 {
-    qDebug () << data;
-    const auto res = http_post (ip_, ("/cgi-bin/push-data" + ("-" + type.toStdString ())).data (), QJsonDocument::fromVariant (data).toJson ().toStdString ());
-    qDebug () << res.data ();
-    return not res.empty ();
+//    qDebug () << data;
+//    const auto res = http_post (ip_, ("/cgi-bin/push-data" + ("-" + type.toStdString ())).data (), QJsonDocument::fromVariant (data).toJson ().toStdString ());
+//    qDebug () << res.data ();
+//    return not res.empty ();
+    return true;
 }
